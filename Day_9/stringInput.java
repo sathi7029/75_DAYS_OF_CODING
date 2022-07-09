1775f117e525fceec659355f713d3f05121316a8
SATHI  RUIDAS <89245802+sathi7029@users.noreply.github.com>
2022-07-09 22:17:33 +0530

    updated

---
+++ Day_9/stringInput.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class stringInput {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String name;
+        System.out.println("may i khow your name:");
+        name=sc.nextLine();
+        System.out.println(name);
+
+    }
+    
+}
