089f2be1aee138cc11c3c3f8f789058cbb637463
SATHI  RUIDAS <89245802+sathi7029@users.noreply.github.com>
2022-07-22 08:00:06 +0530

    updated

---
+++ Day_22/FindFirstAppearsOfElement.java
@@ -0,0 +1,11 @@
+public class FindFirstAppearsOfElement {
+    public static void main(String[] args) {
+        int a[]={5,3,4,5,2,3,4};
+        int res=a[0];
+        for(int i=1;i<a.length;i++)
+        {
+            res=res^a[i];
+        }
+        System.out.println("single element is:"+res);
+    }
+}
