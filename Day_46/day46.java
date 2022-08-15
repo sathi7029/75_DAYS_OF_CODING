76e9a2f944c64e364a4cc66887a8f284099f5806
SATHI  RUIDAS <89245802+sathi7029@users.noreply.github.com>
2022-08-15 21:48:34 +0530

    updated

---
+++ Day_46/day46.java
@@ -0,0 +1,8 @@
+public class day46 {
+    static void savedAday(){
+        System.out.println("just saved a day....");
+    }
+    public static void main(String[] args) {
+        savedAday();
+    }
+}
