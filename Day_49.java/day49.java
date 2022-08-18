de69e1b64994c24652a267088ffa2b1c4dddf78e
SATHI  RUIDAS <89245802+sathi7029@users.noreply.github.com>
2022-08-18 22:02:19 +0530

    updated

---
+++ Day_49.java/day49.java
@@ -0,0 +1,9 @@
+public class day49 {
+    void birthday(){
+        System.out.println("happy  birthday in advance to my best friend haari");
+    }
+    public static void main(String[] args) {
+        day49 b=new day49();
+        b.birthday();
+    }
+}
